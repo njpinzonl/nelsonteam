20e11432756b10f81c810b3b83f495189185e646
Nelson Javier Pinzon Lopez <njpilo@gmail.com>
2018-09-25 10:54:59 -0500

    parece que si

--- server.js
+++ server.js
@@ -14,3 +14,9 @@ res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+// server.js
+const path = require('path');
+app.get('/*all', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
