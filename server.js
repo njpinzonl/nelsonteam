7f1ff1a58e9343affea3d812ca7b71243d2a3aa9
Nelson Javier Pinzon Lopez <njpilo@gmail.com>
2018-09-26 11:18:04 -0500

    thats perfect

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
+const port = process.env.PORT || '8080';
+app.set('port', port);
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/observable-p/index.html'));
-});
-
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
-
-// server.js
-const path = require('path');
-app.get('/*all', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.listen(app.get('port'), function(){
+  console.log("node app is running at localhost:" + app.get('port'));
 });
+
+app.get('/*', function(req,res) {
+  res.set('Content-Type', 'text/html')
+  .sendFile(path.join(__dirname+'/dist/index.html'));
+})
+
+console.log('Console listening!');
+
